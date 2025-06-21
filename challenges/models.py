4e9f717a4614c9ce06a3d463f714abbe898fa355
rimogardino <tiddley@abv.bg>
2025-06-21 15:17:39 +0300

    Finish adding traslate to all text in templates

--- challenges/models.py
+++ challenges/models.py
@@ -1,10 +1,8 @@
 import os
 from math import ceil
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.db import models
-from django.db.models import Q
 from .utils import ChallengeType
 from events.models import Event
 from submissions.models import Submission
@@ -17,7 +15,9 @@ def get_challenge_model_class(challenge_type):
     elif challenge_type == Spot.challenge_type:
         return Spot
     else:
-        raise AssertionError("Provided challenge_type doesn't match any existing challenge type")
+        raise AssertionError(
+            "Provided challenge_type doesn't match any existing challenge type"
+        )
 
 
 # Create your models here.
@@ -27,7 +27,7 @@ class BaseChallenge(models.Model):
     description = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=None, blank=True, null=True)
-    
+
     def approve(self):
         self.approved = True
         self.save()
@@ -54,7 +54,7 @@ class Visual(BaseChallengeInteraction):
         Return the URL for the processed file.
         """
         base, ext = os.path.splitext(self.file.name)
-        if self.file_type.startswith('video'):
+        if self.file_type.startswith("video"):
             processed_name = f"{base}_processed.mp4"
         else:
             processed_name = f"{base}_processed.jpg"
@@ -65,8 +65,8 @@ class Visual(BaseChallengeInteraction):
 
 
 class MapPoint(models.Model):
-    # Latitude is north-south and longitude is east-west, 
-    # so latitude is analogous to Y (up-down) and 
+    # Latitude is north-south and longitude is east-west,
+    # so latitude is analogous to Y (up-down) and
     # longitude is analogous to X (right-left).
     # Latitude is between -90 and 90, longitude is between -180 and 180.
     # https://en.wikipedia.org/wiki/Geographic_coordinate_system
@@ -81,6 +81,7 @@ class MapPoint(models.Model):
 class Spot(BaseChallenge, MapPoint):
     name = models.CharField(max_length=50)
     challenge_type = ChallengeType.SPOT
+
     def __str__(self):
         return f"{self.name}--{self.event}-{self.pk}-: {self.description}"
 
@@ -88,6 +89,7 @@ class Spot(BaseChallenge, MapPoint):
 class Challenge(BaseChallenge, MapPoint):
     points = models.IntegerField(default=1)
     challenge_type = ChallengeType.CHALLENGE
+
     def update(self):
         """
         Updates the state of a challenge.
@@ -108,5 +110,4 @@ class Challenge(BaseChallenge, MapPoint):
         self.save()
 
     def __str__(self):
-        return f'{self.event.name}-{self.user}-id:{self.pk}-points: {self.points}-: {self.description}'
-
+        return f"{self.event.name}-{self.user}-id:{self.pk}-points: {self.points}-: {self.description}"
